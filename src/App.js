16f82b0632e6b566a45a2e3c3d6dc8141b68c945
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-16 12:48:54 +0430

    install react router dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import Home from "./Pages/Home/Home";
-import List from "./Pages/List/List";
-import Login from "./Pages/Login/Login";
-import New from "./Pages/New/New";
-import Single from "./Pages/Single/Single";
 
 function App() {
   return (
     <>
     <div className="app">
       <Home />
-      <List />
-      <Login />
-      <New />
-      <Single />
     </div>
     </>
   );
