14ce81d1654d4581571c53ea2b79e28de6dc928b
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-16 12:31:40 +0430

    create List Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Home from "./Pages/Home/Home";
-
+import List from "./Pages/List/List";
 function App() {
   return (
     <>
     <div className="app">
       <Home />
+      <List />
     </div>
     </>
   );
---
+++ src/Pages/List/List.jsx
@@ -0,0 +1,12 @@
+import "./list.scss"
+import React from "react"
+
+const List = ()=>{
+    return(
+        <div className="list">
+            <h1 className="title">List</h1>
+        </div>
+    )
+}
+
+export default List
