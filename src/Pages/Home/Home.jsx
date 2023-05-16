cafd2d0d0d13f8cd0de37e06d5643a68517c0225
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-16 12:11:59 +0430

    Home Page Inital

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,9 @@ import "./home.scss"
 
 const Home = () => {
     return (
-        <div className="home">Home page</div>
+        <div className="home">
+            <h1 className="title">Fusion Dashboard Pro</h1>
+        </div>
     )
 }
 
