c18bbab777778af53a8fabd5bb0f65c6d7e68920
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-16 15:09:25 +0430

    Create Needed Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login"
+import List from "./Pages/List/List"
+import Single from "./Pages/Single/Single"
+import New from "./Pages/New/New"
 
 function App() {
     return (
     <div className="app">
         <BrowserRouter>
             <Routes>
-               <Route path="/" element={<Home />}></Route>
+               <Route path="/">
+                <Route index element={<Home/>} />
+                <Route path="login" element={<Login/>} />
+                <Route path="users">
+                  <Route index element={<List/>}/>
+                  <Route path=":userid" element={<Single/>} />
+                  <Route path="new" element={<New />} />
+                </Route>
+                <Route path="product">
+                  <Route index element={<List/>}/>
+                  <Route path=":productid" element={<Single/>} />
+                  <Route path="new" element={<New />} />
+                </Route>
+               </Route>
             </Routes>
         </BrowserRouter>
     </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./home.scss"
 const Home = () => {
     return (
         <div className="home">
-            <h1 className="title">Fusion Dashboard Pro</h1>
+            <h1 className="title">Home</h1>
         </div>
     )
 }
