dceff62629b156b4fb7168a3e1506e3425b7899e
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-16 21:39:18 +0430

    Create Sidebar items

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,5 +1,20 @@
 import "./sidebar.scss"
-import {Dashboard} from "@mui/icons-material"
+import 
+{
+    Dashboard,
+    Menu,
+    Group,
+    Store,
+    CreditCard,
+    ExitToApp,
+    AccountCircle,
+    Settings,
+    Psychology,
+    SettingsSystemDaydream,
+    NotificationsNone,
+    InsertChart,
+    LocalShipping
+} from "@mui/icons-material"
 
 
 const Sidebar = () => {
@@ -12,10 +27,54 @@ const Sidebar = () => {
             <hr />
             <div className="center">
                 <ul className="menu">
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
+                    <li>
+                        <Menu />
+                        <span>Dashboard</span>
+                    </li>
+                    <li>
+                        <Group />
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <Store />
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <CreditCard />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShipping />
+                        <span>Delivery</span>
+                    </li>
+                    <li>
+                        <InsertChart />
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsNone />
+                        <span>Notification</span>
+                    </li>
+                    <li>
+                        <SettingsSystemDaydream />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <Psychology />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <Settings />
+                        <span>Setting</span>
+                    </li>
+                    <li>
+                        <AccountCircle />
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <ExitToApp />
+                        <span>Logout</span>
+                    </li>
                 </ul>
             </div>
             <div className="buttom">(color option)</div>
