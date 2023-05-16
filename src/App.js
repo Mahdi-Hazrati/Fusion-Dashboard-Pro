a2c27778cf07a80152a4af2a4178a35ba9d74c5b
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-16 13:02:25 +0430

    Inital React Router Dom and Create first Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 
 function App() {
-  return (
-    <>
+    return (
     <div className="app">
-      <Home />
+        <BrowserRouter>
+            <Routes>
+               <Route path="/" element={<Home />}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
-    </>
-  );
+    );
 }
 
 export default App;
