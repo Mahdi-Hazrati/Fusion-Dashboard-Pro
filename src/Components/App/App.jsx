471e3463d1a9ab226ff21ac89b7f2ebe56151dc9
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-17 21:56:36 +0430

    Move main App component to /Components directory

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,13 +2,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // ----------------- Pages --------------------
-import Home from "./Pages/Home/Home";
-import Login from "./Pages/Login/Login"
-import List from "./Pages/List/List"
-import Single from "./Pages/Single/Single"
-import New from "./Pages/New/New"
+import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login"
+import List from "../../Pages/List/List"
+import Single from "../../Pages/Single/Single"
+import New from "../../Pages/New/New"
 // ---------------- Components ----------------
+// --------------=-- Styles -------------------
+import "./app.scss"
+import "./reset.css"
 // --------------------------------------------
+
 
 function App() {
     return (
