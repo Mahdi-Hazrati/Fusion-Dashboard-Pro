3dd3a194c44dede3944294632d12d0461f9d04e8
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-17 13:42:22 +0430

    Widget static

--- src/Components/Widget/Widget.jsx
+++ src/Components/Widget/Widget.jsx
@@ -1,13 +1,20 @@
 import "./widget.scss"
+import { KeyboardArrowUp, Person } from "@mui/icons-material"
 
 const Widget = ()=>{
     return (
         <div className="widget">
             <div className="left">
-                
+                <div className="title">users</div>
+                <div className="counter">12.000</div>
+                <div className="link">See all users</div>
             </div>
             <div className="right">
-
+                <div className="percentage positive">
+                    <KeyboardArrowUp />
+                    80%
+                </div>
+                <Person  className="icon"/>
             </div>
         </div>
     )
