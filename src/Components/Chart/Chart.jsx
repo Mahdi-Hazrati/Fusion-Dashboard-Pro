e2a1cf1681901b86bd8d5541ea351517aa844f70
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-18 11:39:05 +0430

    install reacharts and make a chart widget in home

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,8 +1,78 @@
 import "./chart.scss"
+import {
+    AreaChart,
+    XAxis,
+    CartesianGrid,
+    Tooltip,
+    Area,
+    ResponsiveContainer
+} from 'recharts';
 
-const Chart = ()=>{
-    return(
-        <div className="chart">Chart</div>
+
+const data = [
+    {
+        name: 'Jan',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Fab',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Mar',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Apr',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'May',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'June',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+];
+
+const Chart = () => {
+    return (
+        <div className="chart">
+            <div className="title">Last 6 month revenue</div>
+            <ResponsiveContainer width="100%" aspect={2/1} className="chartContainer">
+                <AreaChart width={730} height={250} data={data}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    <defs>
+                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="0%" stopColor="crimson" stopOpacity={0.8} />
+                            <stop offset="0%" stopColor="white" stopOpacity={0} />
+                        </linearGradient>
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="0%" stopColor="#3e98c7" stopOpacity={0.8} />
+                            <stop offset="0%" stopColor="white" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="name" stroke="gray"/>
+                    <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="uv" stroke="crimson" fillOpacity={1} fill="url(#colorUv)" />
+                    <Area type="monotone" dataKey="pv" stroke="#3e98c7" fillOpacity={1} fill="url(#colorPv)" />
+                </AreaChart>
+            </ResponsiveContainer>
+        </div>
     )
 }
 
